2e1d6a1f8f23660861e00e52a73cade378250e16
youssef <youssef.hesham.rabea@gmail.com>
2020-05-10 01:35:06 +0200

    Initial commit

---
+++ app/src/main/java/com/delevery/metyassara/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.delevery.metyassara.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.delevery.metyassara.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+    }
+}
