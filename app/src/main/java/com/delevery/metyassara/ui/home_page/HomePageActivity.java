671afc8c8e2a2a3e181dee3c8bf340b09c786e8b
MustafaYounis27 <moustafa.sasa14.mm1@gmail.com>
2020-05-10 21:56:03 +0200

    home fragment package

--- app/src/main/java/com/delevery/metyassara/ui/home_page/HomePageActivity.java
+++ app/src/main/java/com/delevery/metyassara/ui/home_page/HomePageActivity.java
@@ -1,16 +1,41 @@
 package com.delevery.metyassara.ui.home_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.delevery.metyassara.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupDrawer ();
+
+    }
+
+    private void setupDrawer()
+    {
+        DrawerLayout drawerLayout = findViewById ( R.id.drawer_layout );
+
+        NavController navController = Navigation.findNavController ( this, R.id.nav_host_fragment );
+        NavigationView navView = findViewById ( R.id.nav_view );
+        NavigationUI.setupWithNavController ( navView, navController );
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder ( navController.getGraph () )
+                        .setDrawerLayout ( drawerLayout )
+                        .build ();
     }
 }
---
+++ app/src/main/java/com/delevery/metyassara/ui/home_page/MyOrdersFragment.java
@@ -0,0 +1,8 @@
+package com.delevery.metyassara.ui.home_page;
+
+import androidx.fragment.app.Fragment;
+
+public class MyOrdersFragment extends Fragment
+{
+
+}
